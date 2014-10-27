962463126f0131b29a44d09c529fab38105eeffa
Claudio Rodriguez <claudio.rdgz@gmail.com>
2014-10-27 00:58:44 -0600

    initial website

---
+++ script/blog.js
@@ -0,0 +1,10 @@
+var bt = $('.tableofcontents').position().top;
+
+$(window).scroll(function() {
+        var wst = $(window).scrollTop();
+
+        (wst >= bt) ?
+        $('.tableofcontents').css({position: 'fixed', top: 15+'px' }) :  
+        $('.tableofcontents').css({position: 'absolute', top: bt+'px' })
+
+});
