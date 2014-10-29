424b48af2b42a1dabd0aadf4af5e4d0501f4ac88
Claudio Rodriguez <claudio.rdgz@gmail.com>
2014-10-29 11:27:01 -0600

    Changed analytics
    Changed analytics

--- script/blog.js
+++ script/blog.js
@@ -18,7 +18,7 @@ $('[data-toggle=collapse]').click(function(){
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+})(window,document,'script',('https:' == document.location.protocol ? 'https://' : 'http://') +'www.google-analytics.com/analytics.js','ga');
 ga('create', 'UA-35752941-2', 'auto');
 ga('require', 'displayfeatures');
 ga('send', 'pageview');
