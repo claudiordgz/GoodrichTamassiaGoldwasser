d3022b15c1f94fea22adfc951581cd02135c6536
Claudio Rodriguez <claudio.rdgz@gmail.com>
2014-10-30 14:03:24 -0600

    added ads and analytics

--- script/blog.js
+++ script/blog.js
@@ -8,17 +8,3 @@
         // $('.tableofcontents').css({position: 'absolute', top: bt+'px' })
 
 //});
-
-/* swap open/close side menu icons */
-$('[data-toggle=collapse]').click(function(){
-  	// toggle icon
-  	$(this).find("i").toggleClass("glyphicon-chevron-right glyphicon-chevron-down");
-});
-
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script',('https:' == document.location.protocol ? 'https://' : 'http://') +'www.google-analytics.com/analytics.js','ga');
-ga('create', 'UA-35752941-2', 'auto');
-ga('require', 'displayfeatures');
-ga('send', 'pageview');
